85e2bd4bf128ea89ecdfe826dec9b4dff5c8e15c
Jared Lafer <jaredlafer@Jareds-MacBook-Pro.local>
2021-03-27 00:19:51 -0400

    rm config

--- config.py
+++
@@ -1,7 +0,0 @@
-# import logging
-
-# class Config(object):
-
-#     logging.basicConfig(filename='logs/updater.log', 
-#                     level=logging.INFO, 
-#                     format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
